60fa4ca2f2cf7de8585bc3f8657e29e9a8812ed7
theerasakkeng <theerasak_fusrodah@hotmail.com>
2023-08-14 21:21:39 +0700

    add props switch

---
+++ src/stories/Switch.stories.js
@@ -0,0 +1,9 @@
+import MySwitch from "../components/switch.vue"
+
+export default {
+    title: 'Example/Switch',
+    component: MySwitch,
+    tags: ['autodocs'],
+  };
+
+  export const Default = {};
--- src/stories/Toggle.stories.js
+++
@@ -1,9 +0,0 @@
-import Toggle from "../components/toggle.vue"
-
-export default {
-    title: 'Example/Toggle',
-    component: Toggle,
-    tags: ['autodocs'],
-  };
-
-  export const Default = {};
